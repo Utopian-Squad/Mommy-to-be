e3fb1df292a86304322b82b1d02422ed03d9cbac
bini-123 <biniteklu487@gmail.com>
2021-09-04 01:20:11 +0300

    user_model  done

--- src/api/models/userModel.js
+++ src/api/models/userModel.js
@@ -18,3 +18,79 @@
 // });
 
 // module.exports = mongoose.model("User",userSchema);
+const mongoose=require("mongoose");
+const bcrypt = require("bcrypt");
+const mongoosePaginate = require("mongoose-paginate");
+
+const schema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    image: {
+      type: String,
+      default: "default.png",
+    },
+    address: {
+      type: {
+        state: {
+          type: String,
+        },
+        city: {
+          type: String,
+        },
+        woreda: {
+          type: String,
+        },
+      },
+    },
+    dateOfBirth: {
+      type: String,
+    },
+   
+    password: {
+      type: String,
+      select: false,
+    },
+    bloodType: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "BloodType",
+    },
+    role: {
+      type:String,
+      enum:["mom","nurse","admin"],
+        default:"mom"
+    },
+   
+  },
+  {
+    timestamps: true,
+  }
+);
+
+schema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+schema.methods.verifyPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+schema.plugin(mongoosePaginate);
+// schema.methods.
+
+const User = mongoose.model("User", schema);
+
+module.exports = User;
