6bb600347ed46f217ad4fa88abb997a680da742a
bini-123 <biniteklu487@gmail.com>
2021-09-04 16:29:16 +0300

    User router

--- src/api/routes/userRoute.js
+++ src/api/routes/userRoute.js
@@ -1,4 +1,7 @@
 const express = require("express")
+const userValidation = require("../middlewares/user_validation");
+const {verifyUser}  = require("../middlewares/authentication");
+
 const router = express.Router();
 
 const userController = require("../controllers/userController")
@@ -12,6 +15,17 @@ router.post("/",userController.createUser);
 router.put("/:id", userController.updateUser);
 
 router.delete("/:id",userController.deleteUser);
+
+router.get("/",verifyUser,userController.getAllUsers);
+
+router.get("/:id",verifyUser,userController.getOneUser);
+
+router.post("/signup",userValidation.validate("SIGNUP"),userController.createUser);
+router.post("/login",userValidation.validate("LOGIN"),userController.login);
+
+router.patch("/:id", verifyUser,userController.updateUser);
+
+router.delete("/:id",verifyUser,userController.deleteUser);
 
 module.exports = {
     router
