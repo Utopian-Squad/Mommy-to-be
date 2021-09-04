b5cfd78a8b6b85848393610fdac890d736422628
RedietMengiste <se.rediet.mengiste@gmail.com>
2021-09-04 17:02:27 +0300

    final merging

--- src/api/utils/db/db.js
+++ src/api/utils/db/db.js
@@ -6,7 +6,3 @@ MONGODB_URI = "mongodb://localhost:27017/api"
 mongoose.connect(MONGODB_URI, {useNewUrlParser: true},
    ()=>{ console.log("Database Connected");}
     );
-<<<<<<< HEAD
-=======
-
->>>>>>> File Structure
